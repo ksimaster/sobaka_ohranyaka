93942cfcfc1f33c69402b8771b65142962219c27
ksimaster <ksimasterlord@gmail.com>
2020-09-29 19:56:16 +0300

    Скрипт на лай/рычание и др.
    Объединен забор в один объект
    создан GameManager
    создан скрипт на лай и рычание (bool не пошёл, сделано на int)
    добавлена кнопка успокоиться (активна, только когда лай и рычание)

---
+++ Sobaka-Ohranyaka/Assets/Scripts/GameScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+   int activeDog = 0;
+    [SerializeField]
+    Animator animator;
+    [SerializeField]
+    GameObject calmDown;
+
+
+    void Update()
+    {
+        ChangeAnimation();
+        ActiveCalmDownButton();
+        
+    }
+
+    void ChangeAnimation()
+    {
+        animator.SetInteger("activeDog", activeDog);
+    }
+    public void ReturnIdle()
+    {
+        if (activeDog != 0) activeDog = 0;
+        calmDown.SetActive(false);
+    }
+    void ActiveCalmDownButton()
+    {
+        if (activeDog != 0) calmDown.SetActive(true);
+    }
+    public void GrowlButtonClick()
+    {
+        activeDog = 1;
+    }
+
+    public void BarkButtonClick()
+    {
+        activeDog = 2;
+    }
+  
+}
